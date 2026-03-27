858b33383ba527c2c47c54c9c62382c65284fdc0
Gabriela Regina de Souza <122756066+GabrielaSouzaDev@users.noreply.github.com>
2026-03-26 21:52:48 -0300

    Add files via upload

--- visual.py
+++ visual.py
@@ -7,8 +7,16 @@
 # importação da biblioteca
 import customtkinter as ctk
 
+# configurações visuais
+ctk.set_appearance_mode('dark') #Modos 'light' e 'dark'
+ctk.set_default_color_theme('blue') #Temas 'blue', 'dark-blue', 'green'
+
 janela = ctk.CTk()
 janela.geometry('800x600')
 janela.title('Conversor de Temperatura')
+
+# criação de elementos da tela
+entrada = ctk.CTkEntry(janela, placeholder_text='Digite a temperatura em °C: ')
+entrada.pack(pady=20)
 
 janela.mainloop()
