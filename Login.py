1d59ec445d8708ac376a9ead67a9297d04fd067a
Gabriela Regina de Souza <mr27612@gmail.com>
2026-04-28 21:16:41 -0300

    criação de uma nova interface

---
+++ Login.py
@@ -0,0 +1,12 @@
+# Autor: Gabriela Souza
+# Projeto Login
+
+import customtkinter as ctk
+
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('dark-blue')
+
+app = ctk.CTk()
+app.geometry('400x300')
+app.title('Tela de Login')
+
